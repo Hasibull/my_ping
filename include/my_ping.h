e247ce40fe490c0748f785b83d0518f96e14c9bd
Hasibull <hasibul.sbn@gmail.com>
2026-01-05 22:34:44 +0600

    gitignore and cmakefile adjustment

--- include/my_ping.h
+++ include/my_ping.h
@@ -5,6 +5,18 @@
 #ifndef MYPING_H
 #define MYPING_H
 
+#include <stdint.h>
+
+#pragma pack(push, 1)
+typedef struct {
+    uint8_t  type;
+    uint8_t  code;
+    uint16_t checksum;
+    uint16_t id;
+    uint16_t sequence;
+} icmp_hdr_t;
+#pragma pack(pop)
+
 int my_ping(const char *ip_address);
 
 #endif
